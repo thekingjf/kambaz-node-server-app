ec6b88cd3745076edc023f1726887f114c611000
thekingjf <vittiniruiz.j@northeastern.edu>
2025-12-06 20:48:33 -0500

    Moved modules routes and dao

---
+++ Kambaz/Modules/routes.js
@@ -0,0 +1,29 @@
+import ModulesDao from "../Modules/dao.js";
+
+export default function ModulesRoutes(app, db) {
+  const dao = ModulesDao(db);
+
+  const findModulesForCourse = (req, res) => {
+    const { courseId } = req.params;
+    const modules = dao.findModulesForCourse(courseId);
+    res.json(modules);
+  }
+
+  const deleteModule = (req, res) => {
+    const { moduleId } = req.params;
+    const status = dao.deleteModule(moduleId);
+    res.send(status);
+  }
+
+  const updateModule = async (req, res) => {
+    const { moduleId } = req.params;
+    const moduleUpdates = req.body;
+    const status = await dao.updateModule(moduleId,
+                                    moduleUpdates);
+    res.send(status);
+  }
+
+  app.put("/api/modules/:moduleId", updateModule);
+  app.delete("/api/modules/:moduleId", deleteModule);
+  app.get("/api/courses/:courseId/modules", findModulesForCourse);
+}
